abb3fa1f81fec236e2e0e28b433f7c9921e69bc4
Takish Lim <rimutakeshi155@gmail.com>
2024-07-17 16:21:13 +0900

    first commit

---
+++ src/app/[locale]/(authenticated)/dashboardd/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div className="w-[95%] mx-auto min-h-screen">
+      <h2 className="text-miniheader font-semibold mt-8"></h2>
+    </div>
+  );
+};
+
+export default DashboardPage;
